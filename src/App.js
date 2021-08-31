88b7245d528581b8596d762edad1fc1bdc8b704b
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-08-31 20:04:12 +0100

    basic styling done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import Articles from "./components/Articles";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>react-skeleton</h2>
       </header>
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div className="user">
+      <h3>User Details</h3>
+    </div>
+  );
+};
+
+export default User;
